1f9cd1e4fb3828a08acbee3f6e37b8e1b0d9102c
b21627428 <b21627428@cs.hacettepe.edu.tr>
2020-08-11 16:37:33 +0300

    lombokdan kaynaklı hata çözüldü.

--- src/main/java/com/example/demo/dto/person/PersonDTO.java
+++ src/main/java/com/example/demo/dto/person/PersonDTO.java
@@ -1,13 +1,9 @@
 package com.example.demo.dto.person;
 
-import lombok.Data;
-import lombok.ToString;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 
-@Data
-@ToString
+
 public class PersonDTO {
 
     private String identificationNumber;
@@ -21,11 +17,65 @@ public class PersonDTO {
 
     private String phone;
 
+    public PersonDTO(){
+
+    }
     public PersonDTO(String identificationNumber,String email,String name,String address,String phone){
         this.identificationNumber = identificationNumber;
         this.email = email;
         this.name = name;
         this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDTO{" +
+                "identificationNumber='" + identificationNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
